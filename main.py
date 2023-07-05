b20785e51a801ba74c50b65a72d2eb3806a62159
InternSanj <skunkolienkar@epeconsulting.com>
2023-07-05 12:22:27 -0500

    added test code

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 
 import os
 from config.definitions import ROOT_DIR
+import pyPowerGEM.pyTARA as pt
 
 def print_folderstructure_test():
     # Use a breakpoint in the code line below to debug your script.
@@ -14,9 +9,22 @@ def print_folderstructure_test():
     print(__file__)
     print('Input folder path:', os.path.join(ROOT_DIR, 'Input Data'))
 
-
+def tara_test():
+    # This function checks if access to TARA is available on your computer
+    # before v2002.1 the python DLL is required
+    tara = pt.taraAPI(dllFilePath=r'P:\TARA\API\tara.dll')
+    # after v2002.1 the DLL is optional, API uses the installed DLL
+    tara = pt.taraAPI()
+    #Load example case
+    print('TARA API loading case')
+    case_fp = r'P:\TARA\API\examples\pyTARA\example_data\sample2014.raw'
+    print('TARA API example case loaded successfully')
+    tara.loadRawCase(caseFilePath=case_fp, rawVer=32)
+    tara.solveCase()
+    for bus in tara.loopBuses():
+        print(bus)
+    print('TARA API running correctly')
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_folderstructure_test()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    tara_test()
