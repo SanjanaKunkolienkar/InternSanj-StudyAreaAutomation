44393835c9162a0859d7c82498a7bbb8a58b7874
InternSanj <skunkolienkar@epeconsulting.com>
2023-07-14 13:38:10 -0500

    Added function to get county map

--- mapcounty.py
+++ mapcounty.py
@@ -1,11 +1,16 @@
 import os
-import pandas as pd
-import numpy as np
 from config.definitions import ROOT_DIR
 import getcounty as gc
+import geopandas
+import matplotlib.pyplot as plt
 
 def mapcounty(county):
-
+    path_to_data = os.path.join(ROOT_DIR, 'Input Data\TexasCountyMap\Texas Counties Map.geojson')
+    texas = geopandas.read_file(path_to_data)
+    ax = texas.plot(color='white', edgecolor='black')
+    ax.set_axis_off()
+    plt.show()
+    print(texas)
 
 def main():
     county = gc.main()
